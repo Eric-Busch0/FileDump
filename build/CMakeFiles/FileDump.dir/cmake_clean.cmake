e8c616b7cdc75eccb56b206815eb96fa5a660936
EricBuschUTD <emb150530@utdallas.edu>
2021-05-07 21:27:58 -0500

    added cmake config

---
+++ build/CMakeFiles/FileDump.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FileDump.dir/main.c.obj"
+  "FileDump.exe"
+  "FileDump.exe.manifest"
+  "FileDump.pdb"
+  "libFileDump.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FileDump.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
